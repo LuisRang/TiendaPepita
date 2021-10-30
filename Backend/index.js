b894e31c509e64e8c0c3c73b3ab0c08c7eb297f9
LuisRang <luisfrangel94@gmail.com>
2021-10-29 20:06:56 -0500

    Primer Commit

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import db from "./db/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.listen(process.env.PORT, () =>
+    console.log("Backend server runing on port: " + process.env.PORT)
+);
+db.dbConnection();
